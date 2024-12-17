63931020a277b00d50f5fb6f4a2724484140c876
Arthur Fernandes <87949012+artwafam@users.noreply.github.com>
2024-12-17 08:36:52 -0300

    In game Menu working

--- fighting-game-prototype/ui/in-game_menu/pause_menu.gd
+++ fighting-game-prototype/ui/in-game_menu/pause_menu.gd
@@ -1,12 +1,30 @@
 extends Control
 
-@onready var base_ring: BaseRing = $".."
+@onready var base_ring: BaseRing
+@onready var buttons: VBoxContainer = %Buttons
 
 
-func _on_resume_pressed() -> void:
-	base_ring.pause_menu.hide()
-	Engine.time_scale = 1
+func _ready() -> void:
+	# Connects all the main_menu buttons button_got_pressed signals.
+	for button: UIButton in buttons.get_children():
+		print(button)
+		button.button_got_pressed.connect(_menu_button_pressed)
 
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+func init(parent: BaseRing) -> void: 
+	base_ring = parent
+
+
+func _menu_button_pressed(button: UIButton) -> void:
+	match button.button_type:
+		
+		button.ButtonType.RESUME:
+			hide()
+			Global.pause_game(false)
+		
+		button.ButtonType.QUIT_GAME:
+			get_tree().quit()
+		
+		_:
+			if button.scene_path_to_load != "":
+				SceneLoader.load_scene(button.scene_path_to_load)
